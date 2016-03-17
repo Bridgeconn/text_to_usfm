84605105987b2e71225f8408ea37f447a9672f22
uday <udayonrails@hotmail.com>
2016-03-17 15:07:02 +0530

    script change as per file issue

--- text_to_usfm.rb
+++ text_to_usfm.rb
@@ -17,38 +17,38 @@ Dir.glob("**/*.txt") do |file|
 
   output_name = "#{directory_name}/#{File.basename(file_name, '.*')}.usfm"
   output = File.open("#{output_name}", 'w:utf-8')
-  File.open(file_name, "r:utf-8") do |file|
-    book = file.first
-    book_name = "#{book.partition(" ").first}"
 
-    file.each_with_index do |line,i|
-      line.split("\r").each_with_index do |l, i|
-        if l.include? "अध्याय"
-          flag = true
-          l2 = l.partition(" ").last
-          count = 0
-          vers = []
-        end
-        if flag and !(l.include? "अध्याय")
-          count = 1
-          if count != 0
-            vers  << l
-            h.store(l2, vers.delete_if{|x| x == "\n" })
-          end
-        end
-        chap = []
+  text = File.open(file_name, "r:utf-8").read
+  text.gsub!(/\r\n?/, "\n")
+
+  book_name = "#{text.partition(" ").first}"
+
+  text.each_line do |line|
+    line.split("\r").each_with_index do |l, i|
+      if l.include? "अध्याय"
+        flag = true
+        l2 = l.partition(" ").last
+        count = 0
+        vers = []
       end
+      if flag and !(l.include? "अध्याय")
+        count = 1
+        if count != 0
+          vers  << l
+          h.store(l2, vers.delete_if{|x| x == "\n" })
+        end
+      end
+      chap = []
     end
-
   end
   hash.store(book_name, h)
 
   hash.each do|k, v|
     output << "\\id #{k}\n"
     v.each do |k, v|
-      output << "\\c #{k}\n"
+      output << "\\c #{k}"
       v.each do |k, v|
-        output << "\\v #{k}\n"
+        output << "\\v #{k}"
       end
     end
   end
